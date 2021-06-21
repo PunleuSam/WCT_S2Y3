5a91913e765c2c01aeff9c51af1923c8f22332f1
Punleu <stormshadow783@gmail.com>
2021-06-21 15:30:29 +0700

    week9

---
+++ week9/routes/web.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\IndexController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('/home');
+});
+Route::get('/about', function () {
+    return view('/about');
+});
+Route::get('/contact', function () {
+    return view('/contact');
+});
+Route::get('/agent-single', function () {
+    return view('/agent-single');
+});
+Route::get('/agents-grid', function () {
+    return view('/agents-grid');
+});
+Route::get('/blog-grid', function () {
+    return view('/blog-grid');
+});
+Route::get('/blog-single', function () {
+    return view('/blog-single');
+});
+Route::get('/property-grid', function () {
+    return view('/property-grid');
+});
+Route::get('/property-single', function () {
+    return view('/property-single');
+});
